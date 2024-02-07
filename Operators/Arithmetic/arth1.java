72e227e83107e4a1ee571c6461557f384d6fc167
shivamshinde6164 <shivam57s6164@gmail.com>
2024-02-07 20:26:39 +0530

    first arthmetic program

---
+++ Operators/Arithmetic/arth1.java
@@ -0,0 +1,16 @@
+package Operators.Arithmetic;
+
+/**
+ * arth1
+ */
+public class arth1 {
+
+    public static void main(String[] args) {
+        int x = 4;
+        int y = 3;
+        int z = 8;
+
+        int ans = x + y * z + (x - y);
+        System.out.println(ans);
+    }
+}
