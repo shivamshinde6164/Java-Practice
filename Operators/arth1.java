c065e28e63a4f662e6d970d82bc7644dcd7f4935
shivamshinde6164 <shivam57s6164@gmail.com>
2024-02-07 20:57:04 +0530

    Arithmetic operation

--- Operators/arth1.java
+++ Operators/arth1.java
@@ -1,4 +1,4 @@
-package Operators.Arithmetic;
+package Operators;
 
 /**
  * arth1
---
+++ Operators/arth2.java
@@ -0,0 +1,14 @@
+package Operators;
+
+public class arth2 {
+    public static void main(String[] args) {
+        int x = 20;
+        int y = 2;
+
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println(x % y);
+        System.out.println(x / y);
+    }
+}
